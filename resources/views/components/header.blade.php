662a5b48ab371f45cff7ed5823e29f1ec747b2b1
szymon <szymon.kostrubiec@devcomm.pl>
2022-01-11 09:09:08 +0100

    first modular commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<head>
+    <link href="{{ asset('css/material-kit.css') }}" rel="stylesheet">
+    <link hreyf="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet">
+
+    <script type="text/javascript" src="{{ asset('js/material-kit.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/core/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/core/popper.min.js') }}"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>SK-PR0 SHOP</title>
+
+</head>
---
+++ resources/views/mainsite.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+    @include('components.header');
+    <body>
+    @include('components.index.navbar');
+
+    <h1>ELOOO</h1>
+    </body>
+</html>
